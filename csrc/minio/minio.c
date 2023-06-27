176bd1301ebab62a0b1931cddfd29b5f1b2bc9be
Gus Waldspurger <gus@waldspurger.com>
2023-06-27 12:59:36 -0700

    replace locking with atomics

--- csrc/minio/minio.c
+++ csrc/minio/minio.c
@@ -35,40 +35,31 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <stdatomic.h>
 
 #include "../include/uthash.h"
 
-#define CANARY_VALUE (0xD34DB33F)
-#define CHECK_INVARIANTS(cache)                 \
-do {                                            \
-   assert(cache->canary_1 == CANARY_VALUE);     \
-   assert(cache->canary_2 == CANARY_VALUE);     \
-} while (0)
-
 #define AVERAGE_FILE_SIZE (100 * 1024)
 
-#define STAT_INC(cache, field)                                          \
-         pthread_mutex_lock(&cache->stats_lock);                        \
-         cache->field++;                                                \
-         pthread_mutex_unlock(&cache->stats_lock)
+#define STAT_INC(cache, field) atomic_fetch_add(&cache->field, 1)
 
 
 /* Read an item from CACHE into DATA, indexed by FILEPATH, and located on the
    filesystem at FILEPATH. On failure returns ERRNO code with negative value,
    otherwise returns bytes read on success.
    
-   DATA must be block-aligned, in order for direct IO to work properly. */
+   DATA must be block-aligned, in order for direct IO to work properly.
+   
+   Note we use atomics to implement thread safe options because pthreads and
+   traditional synchronization primitives are not safe to use with the Python
+   interpreter, and may cause deadlock to occur, regardless of the correctness
+   of primitives' usage. */
 ssize_t
 cache_read(cache_t *cache, char *filepath, void *data, uint64_t max_size)
 {
-   CHECK_INVARIANTS(cache);
-
-   pthread_mutex_lock(&cache->stats_lock);
    if (cache->n_accs % 1000 == 0) {
       DEBUG_LOG("[MinIO debug] accesses = %lu, hits = %lu, cold misses = %lu, capacity misses = %lu, fails = %lu (usage = %lu/%lu MB) (cache->data = %p) (&cache->used = %p) (pid = %d, ppid = %d)\n", cache->n_accs, cache->n_hits, cache->n_miss_cold, cache->n_miss_capacity, cache->n_fail, cache->used / (1024 * 1024), cache->size / (1024 * 1024), cache->data, &cache->used, getpid(), getppid());
    }
-   pthread_mutex_unlock(&cache->stats_lock);
 
    STAT_INC(cache, n_accs);
 
@@ -76,14 +67,11 @@ cache_read(cache_t *cache, char *filepath, void *data, uint64_t max_size)
    hash_entry_t *entry = NULL;
    HASH_FIND_STR(cache->ht, filepath, entry);
    if (entry != NULL) {
-      /* Don't overflow the buffer. */
-      pthread_rwlock_rdlock(&entry->rwlock);
+      /* Don't overflow the buffer. */                      /* SAFE IF CACHE IS READ-ONLY */
       if (entry->size > max_size) {
-         pthread_rwlock_unlock(&entry->rwlock);
          return -EINVAL;
       }
       memcpy(data, entry->ptr, entry->size);
-      pthread_rwlock_unlock(&entry->rwlock);
 
       STAT_INC(cache, n_hits);
       return entry->size;
@@ -110,34 +98,32 @@ cache_read(cache_t *cache, char *filepath, void *data, uint64_t max_size)
    close(fd);
    if (size <= cache->size - cache->used) {
       /* Acquire an entry. */
-      pthread_mutex_lock(&cache->meta_lock);
-      hash_entry_t *entry = &cache->ht_entries[cache->n_ht_entries++];
-      if (cache->n_ht_entries > cache->max_ht_entries) {
+      size_t n = atomic_fetch_add(&cache->n_ht_entries, 1);
+      if (n >= cache->max_ht_entries) {
          STAT_INC(cache, n_fail);
-         pthread_mutex_unlock(&cache->meta_lock);
          return -ENOMEM;
       }
-      HASH_ADD_STR(cache->ht, filepath, entry);
-      pthread_mutex_unlock(&cache->meta_lock);
+      hash_entry_t *entry = &cache->ht_entries[n];
+      HASH_ADD_STR(cache->ht, filepath, entry);                         /* ??? NOT YET SAFE */
 
-      /* Acquire the writer lock before writing. */
-      pthread_rwlock_wrlock(&entry->rwlock);
+      /* Note there is an implicit assumption that two threads/processes will
+         not simultaneously attempt to access the same filepath for the first
+         time. For ML data-loader applications this is satisfied, since each
+         element is accessed only once per epoch, however this will present a
+         race condition in applications where this scenario can occur. */
 
       /* Copy the filepath into the entry. */
       strncpy(entry->filepath, filepath, MAX_PATH_LENGTH);
       entry->size = size;
 
-      /* Figure out where the data goes. */
-      pthread_mutex_lock(&cache->meta_lock);
-      entry->ptr = cache->data + cache->used;
+      /* Figure out where the data goes. Note that the atomic_X functions return
+         the pre-operation value. */
+      entry->ptr = cache->data + atomic_fetch_add(&cache->used, size);
+
+      /* Copy data to the cache. */
       CHECK_INVARIANTS(cache);
       memcpy(entry->ptr, data, size);
       CHECK_INVARIANTS(cache);
-      cache->used += size;
-      pthread_mutex_unlock(&cache->meta_lock);
-
-      /* Copy data to the cache. */
-      pthread_rwlock_unlock(&entry->rwlock);
 
       STAT_INC(cache, n_miss_cold);
    } else {
@@ -149,31 +135,18 @@ cache_read(cache_t *cache, char *filepath, void *data, uint64_t max_size)
    return size;
 }
 
-/* Clear the cache's hash table and reset used bytes to zero. */
+/* Clear the cache's hash table and reset used bytes to zero.
+
+   Note this function is NOT thread safe, as safety cannot be ensured by the
+   use of atomics alone. Consequently, it must be ensured that the cache is not
+   being actively queried by other threads/processes when it is flushed. */
 void
 cache_flush(cache_t *cache)
 {
-   /* Acquiring the meta lock will prevent N_HT_ENTRIES changing, so using this
-      value as the HT iterator is safe. */
-   pthread_mutex_lock(&cache->meta_lock);
-   size_t old_n_entries = cache->n_ht_entries;
-
-   /* Acquire every entry lock. */
-   for (size_t i = 0; i < old_n_entries; i++) {
-      pthread_rwlock_wrlock(&cache->ht_entries[i].rwlock);
-   }
-   
    /* Clear the HT and the cache metadata. */
    HASH_CLEAR(hh, cache->ht);
-   cache->n_ht_entries = 0;
-   cache->used = 0;
-
-   /* Release every entry lock. */
-   for (size_t i = 0; i < old_n_entries; i++) {
-      pthread_rwlock_unlock(&cache->ht_entries[i].rwlock);
-   }
-
-   pthread_mutex_unlock(&cache->meta_lock);
+   cache->n_ht_entries = 0UL;
+   cache->used = 0UL;
 }
 
 /* Initialize a cache CACHE with SIZE bytes and POLICY replacement policy. On
@@ -186,19 +159,12 @@ cache_init(cache_t *cache, size_t size, policy_t policy)
    cache->used = 0;
    cache->policy = policy;
 
-   cache->canary_1 = CANARY_VALUE;
-   cache->canary_2 = CANARY_VALUE;
-
    /* Zero initial stats. */
    cache->n_accs = 0;
    cache->n_fail = 0;
    cache->n_hits = 0;
    cache->n_miss_capacity = 0;
    cache->n_miss_cold = 0;
-
-   /* Initialize locks. */
-   pthread_mutex_init(&cache->meta_lock, PTHREAD_MUTEX_NORMAL);
-   pthread_mutex_init(&cache->stats_lock, PTHREAD_MUTEX_NORMAL);
 
    /* Initialize the hash table. Allocate more entries than we'll likely need,
       since file size may vary, and entries are relatively small. */
@@ -219,12 +185,6 @@ cache_init(cache_t *cache, size_t size, policy_t policy)
    int max_ht_entries_log2 = 0;
    while (max_ht_entries_copy >>= 1) ++max_ht_entries_log2;
    HASH_MAKE_TABLE(hh, cache->ht, 0, cache->max_ht_entries, max_ht_entries_log2);
-
-   /* Set up each of the HT entries. */
-   for (size_t i = 0; i < cache->max_ht_entries; i++) {
-      hash_entry_t *entry = &cache->ht_entries[i];
-      pthread_rwlock_init(&entry->rwlock, PTHREAD_RWLOCK_DEFAULT_NP);
-   }
 
    /* Allocate the cache's memory, and ensure it's 8-byte aligned so that direct
       IO will work properly. */
