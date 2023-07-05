9966f8e70f27cb6c1977be509000feaa3224c648
Gus Waldspurger <gus@waldspurger.com>
2023-07-05 10:05:05 -0700

    fix test calls to init

--- test/c/test_minio.c
+++ test/c/test_minio.c
@@ -59,7 +59,7 @@ test_timing(size_t cache_size,
 
     /* Cache being tested. */
     cache_t cache;
-    assert(cache_init(&cache, cache_size, POLICY_MINIO) == 0);
+    assert(cache_init(&cache, cache_size, 0, POLICY_MINIO) == 0);
 
     /* Cold accesses. */
     for (int i = 0; i < n_files; i++) {
@@ -118,7 +118,7 @@ test_integrity(size_t cache_size,
 
     /* Cache being tested. */
     cache_t cache;
-    assert(cache_init(&cache, cache_size, POLICY_MINIO) == 0);
+    assert(cache_init(&cache, cache_size, 0, POLICY_MINIO) == 0);
 
     /* Cold accesses. */
     for (int i = 0; i < n_files; i++) {
