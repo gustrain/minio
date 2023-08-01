26af6b404693e5a16e6d2c9faf670d3708e09412
Gus Waldspurger <gus@waldspurger.com>
2023-08-01 15:27:45 -0700

    logging

--- csrc/minio/minio.c
+++ csrc/minio/minio.c
@@ -49,7 +49,7 @@
 bool
 cache_contains(cache_t *cache, char *path)
 {
-   printf("cache: %p\n", cache);
+   printf("cache: %p, entries: %lu max entries: %lu\n", cache, cache->n_ht_entries, cache->max_ht_entries);
 
    hash_entry_t *entry = NULL;
    HASH_FIND_STR(cache->ht, path, entry);
